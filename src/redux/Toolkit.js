2d7270c72b1ea441006a5a1e3dda7fee0db12fc3
nahyun <nhkim0711@gmail.com>
2021-08-23 23:18:20 +0900

    thunk 실패..

---
+++ src/redux/Toolkit.js
@@ -0,0 +1,34 @@
+import { createReducer, configureStore, createAction } from "@reduxjs/toolkit";
+
+export const setName = createAction("SET_NAME");
+export const setGender = createAction("SET_GENDER");
+export const addAnswer = createAction("ADD_ANSWER");
+export const getResult = createAction("GET_RESULT");
+
+const initState = {
+  name: "",
+  gender: "",
+  answer: {},
+  result: {},
+};
+
+const Reducer = createReducer(initState, builder => {
+  builder.addCase(setName, (state, action) => {
+    state.name = action.payload;
+  });
+  builder.addCase(setGender, (state, action) => {
+    state.gender = action.payload;
+  });
+  builder.addCase(addAnswer, (state, action) => {
+    state.answer = { ...state.answer, ...action.payload };
+  });
+  builder.addCase(getResult, (state, action) => {
+    state.result = action.payload;
+  });
+});
+
+const store = configureStore({
+  reducer: Reducer,
+});
+
+export default store;
