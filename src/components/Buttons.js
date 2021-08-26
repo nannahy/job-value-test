3a8e70f686b29007d5f8f3804aca81babffeba5a
nahyun <nhkim0711@gmail.com>
2021-08-26 15:29:00 +0900

    모듈설치

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -5,11 +5,13 @@ const Button = styled.button`
   ${buttonStyle};
   justify-self: center;
   margin: 0 auto;
+  cursor: pointer;
 `;
 
 export const Button2 = styled.button`
   ${buttonStyle};
   width: 200px;
+  cursor: pointer;
 `;
 
 export default Button;
